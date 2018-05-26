76ca8398574d941ba4b50077e235372078ed4059
Vishwa Shah <vishwa@planet.com>
2018-05-26 13:07:20 -0700

    Forms: Added dynamic form for adding routes

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,6 @@ const Header = () => (
 			<ul>
 				<li><Link to='/'>Home</Link></li>
 				<li><Link to='/routes'>routes</Link></li>
-				<li><Link to='/profile'>profile</Link></li>
 			</ul>
 		</nav>
 	</header>
